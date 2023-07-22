611376803609b24421edfefe3556532ca9289de6
Christie Cardoso <christiecardoso@hotmail.com>
2023-07-22 13:02:49 -0300

    criando input email, senha

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,12 +1,17 @@
 "use client";
 
+import Input from '@/components/Input';
 import { Form, Formik } from 'formik'
 import React from 'react'
 
 export default function Login() {
   return (
     <main className="min-h-screen flex items-center justify-center">
-      <Formik>{({ values }) => <Form noValidate>Teste</Form>}</Formik>
-      </main>
+      <Formik>{({ values }) =>
+        <Form noValidate>
+          <Input name="email" type="email" required />
+          <Input name="password" type="password" required autoComplete="off" />
+        </Form>}</Formik>
+    </main>
   )
 }
