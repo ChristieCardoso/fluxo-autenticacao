a631fff703060925567975e149d7004a604a9761
Christie Cardoso <christiecardoso@hotmail.com>
2023-07-22 12:40:05 -0300

    criando pagina de login

---
+++ src/app/login/page.jsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { Form, Formik } from 'formik'
+import React from 'react'
+
+export default function Login() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <Formik>{({ values }) => <Form noValidate>Teste</Form>}</Formik>
+      </main>
+  )
+}
