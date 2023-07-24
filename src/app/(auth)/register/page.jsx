d414eea34a48c371627a4b7868d049a47f1e6bfe
Christie Cardoso <christiecardoso@hotmail.com>
2023-07-23 21:47:14 -0300

    criando o cadastro e rotas

--- src/app/(auth)/register/page.jsx
+++ src/app/(auth)/register/page.jsx
@@ -3,15 +3,20 @@
 import Button from '@/components/Button';
 import Input from '@/components/Input';
 import Link from 'next/link';
-import { Form, Formik } from 'formik'
+import { Form, Formik } from 'formik';
 import * as Yup from "yup";
-import React from 'react'
+import React, { useState } from 'react'
+import { useRouter } from "next/navigation";
 
 export default function Register() {
+  const [error, setError] = useState("");
+  const [isFormSubmitting, setFormSubmitting] = useState(false);
+  const router = useRouter();
 
   const initialValues = {
+    name: "",
     email: "",
-    password: ""
+    password: "",
   };
 
   const validationSchema = Yup.object().shape({
@@ -20,7 +25,44 @@ export default function Register() {
     password: Yup.string().required("O campo senha é obrigatório")
   });
 
-  async function handleSubmit() { }
+  async function handleSubmit(values, { resetForm }) {
+    setFormSubmitting(true);
+    try {
+      await fetch("/api/auth/register", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: values.name,
+          email: values.email,
+          password: values.password,
+        }),
+      }).then(async (res) => {
+        const result = await res.json();
+
+        if (result.status === 201) {
+          alert(result.message);
+          router.push("/login");
+        } else {
+          renderError(result.message);
+          resetForm();
+        }
+        
+        setFormSubmitting(false);
+      });
+    } catch (error) {
+      setFormSubmitting(false);
+      renderError("Erro ao criar conta, tente mais tarde!");
+    }
+  }
+
+  function renderError(msg) {
+    setError(msg);
+    setTimeout(() => {
+      setError("");
+    }, 3000);
+  }
 
   return (
     <main className="min-h-screen flex items-center justify-center">
@@ -29,7 +71,15 @@ export default function Register() {
           <Input name="name" required />
           <Input name="email" type="email" required />
           <Input name="password" type="password" required autoComplete="off" />
-          <Button type="submit" text="Entrar" className="bg-green-500 text-white rounded p-2 cursor-pointer" />
+          <Button
+              type="submit"
+              text={isFormSubmitting ? "Carregando..." : "Inscrever-se"}
+              disabled={isFormSubmitting}
+              className="bg-green-500 text-white rounded p-2 cursor-pointer"
+            />
+            {!values.name && !values.email && !values.password && error && (
+              <span className="text-red-500 text-sm text-center">{error}</span>
+            )}
           <span className="text-xs text-zinc-500">
             Não possui uma conta?
             <strong className="text-zinc-700">
